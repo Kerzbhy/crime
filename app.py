70e0dbe56bb74cece6824145551bae342add7c4e
AdiPutra <adiputra@gmail.com>
2025-11-25 22:35:09 +0800

    a

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from routes.api import cluster_api
 app = Flask(__name__)
 app.register_blueprint(cluster_api, url_prefix='/api')
 
-# --- TAMBAHAN: Agar halaman utama tidak 404 ---
 @app.route('/')
 def index():
-    return "Halo! Server API Crime Zone sudah aktif. Silakan akses via Laravel."
-# -----------------------------------------------
+    return "Server API Crime Zone sudah aktif. Silakan akses via Laravel."
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
